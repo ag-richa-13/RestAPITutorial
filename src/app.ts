5ef84c7c25f9512b6090550fdda71ef2b367e18a
Richa Agrawal <agrawalricha013@gmail.com>
2024-10-18 16:02:12 +0530

    Add Book Model

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import createHttpError, { HttpError } from "http-errors";
 
 import globalErrorHandler from "./middleware/GlobalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 // Global Error Handler
 app.use(globalErrorHandler);
 
